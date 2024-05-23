38b22a5851af191cd5c7def62c686b71267c6367
Avi Sharma <117980764+avisharma444@users.noreply.github.com>
2024-05-24 03:31:33 +0530

    - added Footer - added signin / login LoginPopup - made navbar and card dynamic - completed homepage

--- front-end/src/components/LoginPopup/LoginPopup.jsx
+++ front-end/src/components/LoginPopup/LoginPopup.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/food del assets/frontend_assets/assets'
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
     const [currState,setCurrState] = useState("Sign Up")
   return (
     <div className='LoginPopup'>
       <form action="" className="login-container">
         <div className="login-popup-title">
             <h2>{currState}</h2>
-            <img  onClick={()=>{}} src={assets.cross_icon} alt="" />
+            <img  onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
+        <div className="login-popup-inputs">
+            {currState==="Login"?<></>:<input type="text" placeholder="Your name" required />}
+            {currState==="Login"?<></>:<input type="text" placeholder="Phone Number" required />}
+            {currState==="Login"?<></>:<input type="text" placeholder="Aadhar Card No." required />}
+            {currState==="Login"?<></>:<input type="text" placeholder="Pan Card No." required />}
+            <input type="email" placeholder="Your email" required />
+            <input type="password" placeholder="Password" required />
+        </div>
+        <button>{currState==="Sign Up"?"Create Account":"Login"}</button>
+        <div className="login-popup-condition">
+            <input type="checkbox" required/>
+            <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+        {currState==="Login"?<></>:<p>Already have an Account? <span onClick={()=>setCurrState("Login")}>Login Here</span></p>}
+        {currState==="Sign Up"?<></>:<p>Create a New Account? <span onClick={()=>setCurrState("Sign Up")}>Click Here</span></p>}
+
       </form>
     </div>
   )
